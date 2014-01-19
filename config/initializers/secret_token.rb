32427735fd4fdfad4937f38b86c2e12c181d2b03
drenzulli <dylanr@sas.upenn.edu>
2014-01-19 16:32:05 -0500

    Brand new file... thanks heroku

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+HelloWorld2::Application.config.secret_key_base = '8a2367095afbda6cc517d77d08b96ac7943198c628aec7b58d8ab7eb2903d57ba182e1eaccd3649f0eb9ebe2270be3aea0572fa7a409f59dd70e1f9cb6f80325'
